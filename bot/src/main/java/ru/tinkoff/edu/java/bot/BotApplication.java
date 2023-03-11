6769581dac073ca5acab3820cbf9b360b13ee01b
Pavel <pa11ady@yandex.ru>
2023-03-11 18:26:09 +0300

    Этап 3

---
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/BotApplication.java
@@ -0,0 +1,14 @@
+package ru.tinkoff.edu.java.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotApplication.class, args);
+		System.out.println("Bot");
+	}
+
+}
---
+++ link-parser/src/test/java/ru/tinkoff/edu/java/linkparser/LinkParserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.edu.java.linkparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
