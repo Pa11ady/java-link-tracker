53bbc757caf9362d928e5c3007e45f25b884fe02
Pavel <pa11ady@yandex.ru>
2023-04-09 20:39:18 +0300

    задание 5

---
+++ scrapper/src/test/java/integration/IntegrationTest.java
@@ -0,0 +1,14 @@
+package integration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class IntegrationTest extends IntegrationEnvironment {
+
+    @Test
+    void runContainers() {
+        assertThat(SQL_CONTAINER.isCreated(), equalTo(true));
+    }
+}
